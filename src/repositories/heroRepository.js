848ac30449300ce883325f8691bb899cca8b71b1
Francisco Viana <franciscoviana862@gmail.com>
2020-12-12 19:43:59 -0300

    repository

--- src/repositories/heroRepository.js
+++ src/repositories/heroRepository.js
@@ -15,12 +15,24 @@ class HeroRepository {
       
       return all.find(({ id }) => itemId === id);
     }
+
+    async create(data) {
+      const currentFile = await this.__currentFileContent();
+      currentFile.push(data);
+
+      await writeFile(this.file, JSON.stringify(currentFile));
+
+      return data.id;
+    }
 }
 
 module.exports = HeroRepository;
 
-const heros = new HeroRepository({
-  file: './../../src/database/data.json'
-});
+// const heros = new HeroRepository({
+//   file: './../../src/database/data.json'
+// });
 
-heros.find(1).then(console.log).catch(err => console.log(err))
+// heros.find(1).then(console.log).catch(err => console.log(err))
+// heros.create({ id: 2, name: 'Homem de Ferro'})
+//   .then(console.log)
+//   .catch(err => console.log(err))
