3dfcedf096d5788467d254c8b216003bb2884650
Francisco Viana <franciscoviana862@gmail.com>
2023-05-06 16:24:30 -0300

    refactor

--- src/index.js
+++ src/index.js
@@ -1,72 +1,78 @@
-const http = require('http');
+const http = require("http");
 const PORT = 3000;
-const DEFAULT_HEADER = { 'Content-Type': 'application/json' };
-const HeroFactory = require('./factories/heroFactory');
+const DEFAULT_HEADER = { "Content-Type": "application/json" };
+const HeroFactory = require("./factories/heroFactory");
 const heroService = HeroFactory.generateInstance();
-const Hero = require('./entities/hero');
+const Hero = require("./entities/hero");
 
 const routes = {
-    '/heroes:get': async (request, response) => {
-        const { id } = request.queryString;
-        const heroes = await heroService.find(id);
+  "/heroes:get": async (request, response) => {
+    const { id } = request.queryString;
+    const heroes = await heroService.find(id);
 
-        response.write(JSON.stringify({ results: heroes }));
-        
-        return response.end();
-    },
-    '/heroes:post': async (request, response) => {
-       // async iterator
-       for await ( const data of request) {
-        try {
-            // await Promise.reject('/heroes:get')
-            const item = JSON.parse(data);
-            const hero = new Hero(item);
-            const { error, valid } = hero.isValid();
-    
-            if(!valid) {
-                response.writeHead(400, DEFAULT_HEADER);
-                response.write(JSON.stringify({ error: error.join(', ') }));
-    
-                return response.end();
-            }
-    
-            const id = await heroService.create(hero);
-            response.writeHead(201, DEFAULT_HEADER);
-            response.write(JSON.stringify({ success: 'User Created with success!!', id }));
-    
-            return response.end();
-        } catch (error) {
-            return handlerError(response)(error);
+    response.write(JSON.stringify({ results: heroes }));
+
+    return response.end();
+  },
+  "/heroes:post": async (request, response) => {
+    // async iterator
+    for await (const data of request) {
+      try {
+        // await Promise.reject('/heroes:get')
+        const item = JSON.parse(data);
+        const hero = new Hero(item);
+        const { error, valid } = hero.isValid();
+
+        if (!valid) {
+          response.writeHead(400, DEFAULT_HEADER);
+          response.write(JSON.stringify({ error: error.join(", ") }));
+
+          return response.end();
         }
-       }
-    },
-    default: (request, response) => {
-        response.write('Hello!');
-        response.end();
+
+        const id = await heroService.create(hero);
+        response.writeHead(201, DEFAULT_HEADER);
+        response.write(
+          JSON.stringify({ success: "User Created with success!!", id })
+        );
+
+        return response.end();
+      } catch (error) {
+        return handlerError(response)(error);
+      }
     }
+  },
+  default: (request, response) => {
+    response.write("Hello!");
+    response.end();
+  },
 };
 
-const handlerError = response => {
-    return error => {
-        console.log('Deu Ruim***', error);
-        response.writeHead(500, DEFAULT_HEADER);
-        response.write(JSON.stringify({ error: 'Internal Server Error!! '}));
+const handlerError = (response) => {
+  return (error) => {
+    console.log("Deu Ruim***", error);
+    response.writeHead(500, DEFAULT_HEADER);
+    response.write(JSON.stringify({ error: "Internal Server Error!! " }));
 
-        return response.end();
-    }
+    return response.end();
+  };
 };
 
 const handler = (request, response) => {
-    const { url, method } = request;
-    const [first, route, id] = url.split('/');
-    request.queryString = { id: isNaN(id) ? id : Number(id)};
-    
-    const key = `/${route}:${method.toLowerCase()}`
+  const { url, method } = request;
+  console.log('url', url);
+  const [first, route, id] = url.split("/");
+  request.queryString = { id: isNaN(id) ? id : Number(id) };
 
-    response.writeHead(200, DEFAULT_HEADER);
+  const key = `/${route}:${method.toLowerCase()}`;
+  console.log('key', key);
 
-   const chosen = routes[key] || routes.default;
-   return chosen(request, response).catch(handlerError(response));
+  response.writeHead(200, DEFAULT_HEADER);
+
+  const chosen = routes[key] || routes.default;
+  return chosen(request, response).catch(handlerError(response));
 };
 
-http.createServer(handler).listen(PORT, () => console.log('server running at', PORT));
+http
+  .createServer(handler)
+  .listen(PORT, () => console.log("server running at", PORT));
